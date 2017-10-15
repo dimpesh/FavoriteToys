ac6249296be38f5868c1f5aaa64eefb48ff7ec8c
dk_2094 <kdimpesh@gmail.com>
2017-10-15 12:17:50 +0530

    1.02 ScrollView added

--- app/src/main/java/com/exxample/android/favoritetoys/MainActivity.java
+++ app/src/main/java/com/exxample/android/favoritetoys/MainActivity.java
@@ -1,14 +1,22 @@
 package com.exxample.android.favoritetoys;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView toyView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        toyView = (TextView) findViewById(R.id.tv_toy_names);
+        //toyView.setText("");
+        for(String toyNames : ToyBox.getToyNames()){
+            String prevText =toyView.getText().toString();
+            toyView.setText(prevText+"\n\n\n"+toyNames);
+        }
+
     }
 }
